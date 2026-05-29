e1139625219d54cddb8ff3c78398c661911c2727
Random Dent <random.dent@gmx.net>
2026-05-29 08:28:09 +0200

    Fundtyp: Retreat ergänzt

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    CACHE_VERSION bei jedem Update hochzählen!
    ========================================================================== */
 
-const CACHE_VERSION = 'aralog-v39';
+const CACHE_VERSION = 'aralog-v40';
 const MAP_CACHE = 'aralog-map-tiles-v1';
 const MAP_CACHE_LIMIT = 500; // max cached tiles
 
